075cc897569e3af9d505a1976c8775152d52262a
arlenochka <m.nupreichik@yandex.ru>
2024-04-13 23:34:20 +0300

    API yandex maps implemented

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy as alch
 
 app = Flask(__name__)
 
 # декоратор - обработчик
 # главная страница
 @app.route("/index")
-@app.route("/") 
+@app.route("/")
 def index():
     return render_template('index.html')
 
 @app.route("/about") 
 def about():
     return render_template('about.html')
+
+# работа с DB Postgres
+# engine = alch.create_engine("postgresql+psycopg2://root:pass@localhost/mydb")
+
 
 # файл основной для данного приложения
 if __name__ == '__main__':
